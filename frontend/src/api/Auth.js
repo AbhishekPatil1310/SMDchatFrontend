c3cd93825dae036b2a771f5f767555b24d7cbeb6
AbhishekPatil1310 <ap7497493@gmail.com>
2025-09-25 20:33:21 +0530

    Update Auth.js

--- frontend/src/api/Auth.js
+++ frontend/src/api/Auth.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/auth",
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/auth`,
     withCredentials: true, // allows cookies (refresh token)
 });
 
 export const signup = (data) => API.post("/signup", data);
 export const signin = (data) => API.post("/signin", data);
 export const refreshToken = () => API.post("/refresh");
+
 export const logout = () => API.post("/logout");
